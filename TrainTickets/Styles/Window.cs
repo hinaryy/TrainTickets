5f72d6c449bb6c9b0e4451dd4c20098edd5eac44
hinaryy <shpak1352@gmail.com>
2023-05-10 12:24:58 +0300

    init project

---
+++ TrainTickets/Styles/Window.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TrainTickets.Styles
+{
+    partial class Window
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+        private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestoreButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                var window = (System.Windows.Window)((FrameworkElement)sender).TemplatedParent;
+
+                window.DragMove();
+            }
+        }
+
+    }
+}
