1c9c5f984dd96d63727b77aa6a23bc4befc7d990
hinaryy <shpak1352@gmail.com>
2023-05-12 08:42:04 +0300

    vm

---
+++ TrainTickets/ViewModel/SignInViewModel.cs
@@ -0,0 +1,54 @@
+﻿using TrainTickets.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace TrainTickets.ViewModel
+{
+    public class SignInViewModel : ViewModelBase
+    {
+        private string _login;
+        private string _password;
+
+        public string Login 
+        { 
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login));  
+            }
+        }
+        public string Password 
+        { 
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));   
+            } 
+        }
+
+        public ICommand SignInCommand { get; }
+
+        public SignInViewModel() 
+        {
+            SignInCommand = new ViewModelCommand(executeAction, canExecuteAction);
+        }
+
+        public bool canExecuteAction(object obj)
+        {
+            return true;
+        }
+
+        public void executeAction(object obj)
+        {
+            MessageBox.Show("Пароль: " + Password);
+        }
+
+    }
+}
